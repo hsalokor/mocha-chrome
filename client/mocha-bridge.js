b1a71788efd9ac28e56f371454d8a3f700336ecf
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2017-11-14 19:36:23 +0200

    WIP: still more experimentation with xunit reporter

--- client/mocha-bridge.js
+++ client/mocha-bridge.js
@@ -4,10 +4,13 @@
   const pick = (o, ...props) => Object.assign({}, ...props.map(prop => ({[prop]: o[prop]})));
 
   const retransmit = (runner, event, ...props) => {
-    runner.on(event, (data, details) => {
-      const filteredData = data ? pick(data, ...props) : {};
+    runner.on(event, (data, err) => {
+      const d = data ? pick(data, ...props) : {};
+      const parent = data && data.hasOwnProperty('parent') ? pick(data.parent, ...props) : null;
+      const filteredData = Object.assign({}, d, { parent, err: err ? err : {} });
       const keys = data ? Object.keys(data) : [];
-      window._eventbus.emit('mocha-event', { event, keys, data: filteredData, details });
+      const parentKeys = data && data.hasOwnProperty('parent') ? Object.keys(data.parent) : [];
+      window._eventbus.emit('mocha-event', { event, keys, parentKeys, data: filteredData, err });
     });
   };
 
@@ -25,7 +28,7 @@
     retransmit(runner, 'test end', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
     retransmit(runner, 'pending', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
 
-    retransmit(runner, 'fail', 'title', 'fn', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', 'callback', 'duration', 'state', '_trace');
+    retransmit(runner, 'fail', 'title', 'err', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', 'callback', 'duration', 'state', '_trace');
     retransmit(runner, 'pass', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', 'callback', 'duration', 'state');
 
     retransmit(runner, 'end');
