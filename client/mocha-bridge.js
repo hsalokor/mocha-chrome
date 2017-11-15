b241dfbe8005fe2a3ceb2cab9256b32d8e108601
Harri Salokorpi <harri.salokorpi@reaktor.fi>
2017-11-15 14:20:09 +0200

    WIP: use circularjson

--- client/mocha-bridge.js
+++ client/mocha-bridge.js
@@ -1,16 +1,12 @@
 (function (root) {
   'use strict';
 
-  const pick = (o, ...props) => Object.assign({}, ...props.map(prop => ({[prop]: o[prop]})));
-
-  const retransmit = (runner, event, ...props) => {
+  const retransmit = (runner, event) => {
     runner.on(event, (data, err) => {
-      const d = data ? pick(data, ...props) : {};
-      const parent = data && data.hasOwnProperty('parent') ? pick(data.parent, ...props) : null;
-      const filteredData = Object.assign({}, d, { parent, err: err ? err : {} });
       const keys = data ? Object.keys(data) : [];
       const parentKeys = data && data.hasOwnProperty('parent') ? Object.keys(data.parent) : [];
-      window._eventbus.emit('mocha-event', { event, keys, parentKeys, data: filteredData, err });
+      const serializedData = data ? CircularJSON.stringify(data) : CircularJSON.stringify({});
+      window._eventbus.emit('mocha-event', { event, keys, parentKeys, data : serializedData, err });
     });
   };
 
@@ -18,18 +14,18 @@
   root._mochabridge = function (runner) {
     retransmit(runner, 'start');
 
-    retransmit(runner, 'suite', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
-    retransmit(runner, 'suite end', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
+    retransmit(runner, 'suite');
+    retransmit(runner, 'suite end');
 
-    retransmit(runner, 'hook', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_slow', '_trace');
-    retransmit(runner, 'hook end', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
+    retransmit(runner, 'hook');
+    retransmit(runner, 'hook end');
 
-    retransmit(runner, 'test', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
-    retransmit(runner, 'test end', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
-    retransmit(runner, 'pending', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', '_trace');
+    retransmit(runner, 'test');
+    retransmit(runner, 'test end');
+    retransmit(runner, 'pending');
 
-    retransmit(runner, 'fail', 'title', 'err', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', 'callback', 'duration', 'state', '_trace');
-    retransmit(runner, 'pass', 'title', 'body', 'async', 'sync', 'timedOut', 'pending', 'type', 'file', 'callback', 'duration', 'state');
+    retransmit(runner, 'fail');
+    retransmit(runner, 'pass');
 
     retransmit(runner, 'end');
   };
